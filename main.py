dbba1070a6a9b62b2b8380d4ecaa7aafd7e86511
ldisse <ldisse@ethz.ch>
2020-03-20 23:08:14 +0100

    mainfile update

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import data_scheduler.lib_data_merger as mice_data
+from FeatureExtraction import getFeatures
 
 if __name__=='__main__':
-    mice_data_dir = r'C:\Users\lkokot\Desktop\ETHZ_STAT_MSC\sem_2\stats_lab\analysis\CSV data files for analysis'
+    mice_data_dir = r'/Users/lucadisse/ETH/Master/FS20/StatsLab/CSV data files for analysis'
     md = mice_data.MiceDataMerger(mice_data_dir)
     data = md.fetch_mouse_signal(165, 'eth', 'running')
-    print(data.get_pandas()[:35])
-    sliced_data = data.sliced_data(12)
-    print(len(sliced_data))
+
+    #for feature extraction time should always be true
+    sliced_data = data.sliced_data(time=True, slice_min=30)
+    features = getFeatures(sliced_data, print_feat=True)
