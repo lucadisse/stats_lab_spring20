ede04daac52a28f768637bb02b706fa0971890c7
ldisse <ldisse@ethz.ch>
2020-03-23 17:30:35 +0100

    optional feature selection

--- Main.py
+++ Main.py
@@ -14,12 +14,13 @@ if __name__=='__main__':
     #data_glu_run = md.fetch_mouse_signal(165, 'sal', 'running')
     #data_sal_run = md.fetch_mouse_signal(165, 'nea', 'running')
 
-    #features = getfewFeatures(data_eth_run, print_feat=True)
-    #print(features.shape[1])
+    #features = getfewFeatures(data_eth_run, signal="running", print_feat=True)
+    #print(features.columns)
 
     #plotFeatures(md)
 
     features = getallFeatures(md, signal='running', slice_min=30)
+    print(features)
     #features[features['treatment'] == "glu"].plot(subplots=True, sharex=True, figsize=(10, 10))
     #plt.show()
     #print(features.columns)
